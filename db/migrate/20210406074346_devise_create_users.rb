63bb9d17ee94ad87cc07c109652a9047fea0f87b
rache930 <ryutaro.093031@gmail.com>
2021-04-12 12:59:34 +0900

    ユーザーテーブルの変更

--- db/migrate/20210406074346_devise_create_users.rb
+++ db/migrate/20210406074346_devise_create_users.rb
@@ -6,9 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      # t.string :name,               null: false
-      # t.text :profile,              null: false
-      # t.string :number,             null: false
+      t.string :name,               null: false
+      t.text :profile,              null: false
+      t.string :number,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -41,7 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :number,               unique: true
+    add_index :users, :number,               unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,16 @@ ActiveRecord::Schema.define(version: 2021_04_06_080344) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.string "number", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["number"], name: "index_users_on_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
